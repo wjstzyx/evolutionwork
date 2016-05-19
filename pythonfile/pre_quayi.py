d28354499ae597378a2c97fe970b0a05df0174fc
wjstzyx <yuyang_998@sina.com>
2016-05-19 11:29:37 +0800

    yy update quanyi

--- pythonfile/pre_quayi.py
+++ pythonfile/pre_quayi.py
@@ -68,29 +68,18 @@ def main_pre_quanyi():
 	IClist=('YEEXIC','YEOTIC','YETRIC','YEZHIC')
 	IFlist=('9CPPUD','DayBrIF','9Distance','9DUD1','9DUDHL','9DUDRV','9EXR1410','9EXV1410','9FORCE','9FQS1','9HAL','9HAL2','9HAL3','9HALMA','9HUITIAO','9HUITIAO2','9HUITIAO3','9HUITIAO4','9KDHDAY','9KDHPM','9Linerate','9RATE','9MALONGK','9MiddayTrend','9MinVolPbuy','9MONDAY','9MORNINGOUT','9MT','9MVRATE','9NHL','9NOON','9OpenBet','9QMA','9QPMIF','9Reversal','9Reversal2','9Reversal3','9V4EIV','9VK1','9VK3','9VPINVOl_L','9VPINVOl_S','9VPINVOl_S2','9wb','9weipan','9WeipanREV','9WeipanStatics','9YAP01','9YY2','9YYMA','9LUD','9LUD2CH','9LUD3','9LUD4','9LUD5','9LUD6','9LUD7','9LUD8','9LUD10','9LUD11V2','9LUD13','9LUD14','9LUD16','LUDch1','LUDch4','LUDch5','LUDch6','LUDch8','LUD52015','LUD62015','LUD72015','LUD82015','TimeV2Pm','TimeV3DtaPm','TimeV3HLAm','TimeV3HLPm','V4EIVelements','V4EIVNEW','IFQG1310','IFQGEX','IFQGOT','9QGBombma2','IFQGTB','IFQGTR','IFQGWB','YEQGTR','YEQGEX','YEQGOT','YEQGZH211')
 
-	# pre_data_for_ac(RBlist,'RB')
-	# pre_data_for_ac(CUlist,'CU')
-	# pre_data_for_ac(AGlist,'AG')
-	# pre_data_for_ac(IClist,'IC')
-	# pre_data_for_ac(IFlist,'IF')
-	# pre_data_for_ac(RUlist,'RU')
-	# pre_data_for_ac(TAlist,'TA')
+	pre_data_for_ac(RBlist,'RB')
+	pre_data_for_ac(CUlist,'CU')
+	pre_data_for_ac(AGlist,'AG')
+	pre_data_for_ac(IClist,'IC')
+	pre_data_for_ac(IFlist,'IF')
+	pre_data_for_ac(RUlist,'RU')
+	pre_data_for_ac(TAlist,'TA')
 
 
 
-print "ddd"
-sql="select 1"
-res=ms.dict_sql(sql)
-print res
+# print "ddd"
+# sql="select 1"
+# res=ms.dict_sql(sql)
+# print res
 main_pre_quanyi()
-# pre_quanyi_data('DayBrIF','IF',0)
-# pre_quanyi_data('DayBrIF','IF',1)
-# pre_quanyi_data('LUDch1','IF',0)
-# pre_quanyi_data('LUDch1','IF',1)
-# pre_quanyi_data('LUDch4','IF',0)
-# pre_quanyi_data('LUDch4','IF',1)
-# pre_quanyi_data('LUDch5','IF',0)
-# pre_quanyi_data('LUDch5','IF',1)
-# pre_quanyi_data('LUDch6','IF',0)
-# pre_quanyi_data('LUDch6','IF',1)
-# pre_quanyi_data('LUDch8','IF',0)
