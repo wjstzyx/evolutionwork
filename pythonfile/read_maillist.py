36d25cf45638ad8f831cec71a1b7ac684f6bdaca
wjstzyx <yuyang_998@sina.com>
2016-05-25 14:14:07 +0800

    1

--- pythonfile/read_maillist.py
+++ pythonfile/read_maillist.py
@@ -14,25 +14,22 @@ ms = MSSQL(host="192.168.0.5",user="future",pwd="K@ra0Key",db="LogRecord")
 def read_maillist():
 	sql="select * from [LogRecord].[dbo].[maillist] where type !=1 "
 	res=ms.dict_sql(sql)
-	if res:
-		mailtolist=res[0]['mailtolist'].split(',')
+	for item in res:
+		mailtolist=item['mailtolist'].split(',')
 		print mailtolist
 		totalmsg=''
-		idlist=[]
 		for item in res:
 			msg=item['msg']
-			idlist.append(item['id'])
-			totalmsg=totalmsg+'<br>'+item['inserttime'].strftime('%Y-%m-%d %H-%M-%S')+'<br>subject : '+item['subject']+'<br>message: '+item['msg']+'</br>'
+			subject=item['subject']+datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+			totalmsg=totalmsg+'<br>'+item['inserttime'].strftime('%Y-%m-%d %H-%M-%S')+'<br>message: '+item['msg']+'</br>'
 		print totalmsg
-		if send_mail(mailtolist,"夜盘报警提醒",totalmsg):
-			for item in idlist:
-				sql="update [LogRecord].[dbo].[maillist] set type=1,updatetime=getdate() where id=%s" % (item)
-				ms.insert_sql(sql)
+		if send_mail(mailtolist,subject,totalmsg):
+			sql="update [LogRecord].[dbo].[maillist] set type=1,updatetime=getdate() where id=%s" % (item['id'])
+			ms.insert_sql(sql)
 			print "发送成功"  
 		else:
-			for item in idlist:
-				sql="update [LogRecord].[dbo].[maillist] set type=2,updatetime=getdate() where id=%s" % (item)
-				ms.insert_sql(sql)
+			sql="update [LogRecord].[dbo].[maillist] set type=2,updatetime=getdate() where id=%s" % (item['id'])
+			ms.insert_sql(sql)
 			print "发送失败"
 
 
--- pythonfile/sendmail.py
+++ pythonfile/sendmail.py
@@ -1,6 +1,7 @@
 #coding=utf-8 
 #!/usr/bin/env python
 import sys
+import datetime
 reload(sys)
 sys.setdefaultencoding('utf8')
 from dbconn import MSSQL
@@ -10,24 +11,32 @@ ms = MSSQL(host="192.168.0.5",user="future",pwd="K@ra0Key",db="future")
 # -*- coding: UTF-8 -*-
 import smtplib  
 from email.mime.text import MIMEText  
-# mailto_list=['yuyang@evolutionlabs.com.cn'] 
-# mail_host="smtp.evolutionlabs.com.cn"  #设置服务器
-# mail_user="warning@evolutionlabs.com.cn"    #用户名
-# mail_pass="El104104"   #口令 
-# mail_postfix="evolutionlabs.com.cn"  #发件箱的后缀
+# mailto_list=['yuyang_998@sina.com'] 
+mailto_list=['794513386@qq.com','yuyang@evolutionlabs.com.cn'] 
+mail_host="smtp.evolutionlabs.com.cn"  #设置服务器
+mail_user="warning@evolutionlabs.com.cn"    #用户名
+mail_pass="El104104"   #口令 
+mail_postfix="evolutionlabs.com.cn"  #发件箱的后缀
+sendmail='warning@evolutionlabs.com.cn'
+#rwecstmzqbwzbchh
+# mailto_list=['yuyang_998@sina.com'] 
+# mail_host="smtp.qq.com"  #设置服务器
+# mail_user="794513386@qq.com"    #用户名
+# mail_pass="rwecstmzqbwzbchh"   #口令 
+# mail_postfix="qq.com"  #发件箱的后缀
+# sendmail='794513386@qq.com'
 
-mailto_list=['yuyang@evolutionlabs.com.cn','794513386@qq.com'] 
-mail_host="smtp.sina.com"  #设置服务器
-mail_user="elwarning"    #用户名
-mail_pass="qwe123"   #口令 
-mail_postfix="sina.com"  #发件箱的后缀
-sendmail='elwarning@sina.com'
-
+# mailto_list=['elwarning@sina.com'] 
+# mail_host="smtp.sina.com"  #设置服务器
+# mail_user="elwarning"    #用户名
+# mail_pass="qwe123"   #口令 
+# mail_postfix="sina.com"  #发件箱的后缀
+# sendmail='elwarning@sina.com'
 
 def send_mail(to_list,sub,content):  
-    me="elwarning"+"<"+mail_user+"@"+mail_postfix+">"  
-    # me=sendmail
-    #msg = MIMEText(content,_subtype='plain',_charset='utf8')
+    #me="elwarning"+"<"+mail_user+"@"+mail_postfix+">"  
+    me=sendmail
+    # msg = MIMEText(content,_subtype='plain',_charset='utf8')
     msg = MIMEText(content,_subtype='html',_charset='utf8')  
     msg['Subject'] = sub  
     msg['From'] = me  
@@ -43,7 +52,9 @@ def send_mail(to_list,sub,content):
         print str(e)  
         return False  
 if __name__ == '__main__':  
-    if send_mail(mailto_list,"测试报警","这是一封测试邮件，请忽略"):  
+    time=datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+    if send_mail(mailto_list,"测试报警%s" % (time),"这是一封测试邮件，请忽略 %s" % (time)):  
+        print "这是一封测试邮件，请忽略 %s" % (time)
         print "发送成功"  
     else:  
         print "发送失败"
