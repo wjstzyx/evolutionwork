365a5476d8ce4a368246eff7087bee9ed00473ae
wjstzyx <yuyang_998@sina.com>
2016-06-02 10:59:51 +0800

    1

--- pythonfile/sendmailshouhu.py
+++ pythonfile/sendmailshouhu.py
@@ -9,12 +9,12 @@ ms = MSSQL(host="192.168.0.5",user="future",pwd="K@ra0Key",db="future")
 # resList = ms.find_sql("select top 2 * from st_report")
 # print resList
 
-cmd="ps -ef|grep 'read_maillist.py'|grep -v 'grep'"
+cmd="ps -ef|grep 'read_maillist.py'|grep -v 'grep'|awk '{print $2}'"
 output=os.popen(cmd)
 res=output.read()
 print res,type(res)
 if res:
-	record=res.split(' ')
+	processid=res
 	print record
 if res=='':
 	cmd='python /home/yuyang/myfile/evolutionwork/pythonfile/read_maillist.py &'
