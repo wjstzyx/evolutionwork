fc05ee2126c56d4db0025e4ef0337c75edd88bdc
wjstzyx <yuyang_998@sina.com>
2016-06-20 14:06:23 +0800

    1

--- pythonfile/real_quayi_daily.py
+++ pythonfile/real_quayi_daily.py
@@ -218,6 +218,12 @@ def main_pre_quanyi():
 	IFlist=('9CPPUD','DayBrIF','9Distance','9DUD1','9DUDHL','9DUDRV','9EXR1410','9EXV1410','9FORCE','9FQS1','9HAL','9HAL2','9HAL3','9HALMA','9HUITIAO','9HUITIAO2','9HUITIAO3','9HUITIAO4','9KDHDAY','9KDHPM','9Linerate','9RATE','9MALONGK','9MiddayTrend','9MinVolPbuy','9MONDAY','9MORNINGOUT','9MT','9MVRATE','9NHL','9NOON','9OpenBet','9QMA','9QPMIF','9Reversal','9Reversal2','9Reversal3','9V4EIV','9VK1','9VK3','9VPINVOl_L','9VPINVOl_S','9VPINVOl_S2','9wb','9weipan','9WeipanREV','9WeipanStatics','9YAP01','9YY2','9YYMA','9LUD','9LUD2CH','9LUD3','9LUD4','9LUD5','9LUD6','9LUD7','9LUD8','9LUD10','9LUD11V2','9LUD13','9LUD14','9LUD16','LUDch1','LUDch4','LUDch5','LUDch6','LUDch8','LUD52015','LUD62015','LUD72015','LUD82015','TimeV2Pm','TimeV3DtaPm','TimeV3HLAm','TimeV3HLPm','V4EIVelements','V4EIVNEW','IFQG1310','IFQGEX','IFQGOT','9QGBombma2','IFQGTB','IFQGTR','IFQGWB','YEQGTR','YEQGEX','YEQGOT','YEQGZH211')
 	ICIFlist=('YEQGEX','YEQGOT','YEQGTR')
 
+	sql="select getdate()"
+	res=ms.find_sql(sql)[0][0]
+	res=res.strftime("%Y-%m-%d %H:%M:%S")
+	newD=res[2:4]+res[5:7]+res[8:10]
+	sql="delete from real_dailyquanyi where D=%s" % (newD)
+	ms.insert_sql(sql)
 	pre_data_for_ac(RBlist,'RB')
 	pre_data_for_ac(CUlist,'CU')
 	pre_data_for_ac(AGlist,'AG')
