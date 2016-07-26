8c5a6a77d9dfc552d9efc5167e2cd9bbe178662f
wjstzyx <yuyang_998@sina.com>
2016-07-26 16:09:33 +0800

    1

--- mytest/aa.py
+++ mytest/aa.py
@@ -1,15 +1,118 @@
-#coding=utf-8 
-#!/usr/bin/env python
+# -*- coding: gb18030 -*- 
+import string, os
+import shutil
+from dbconn import MSSQL
+ms = MSSQL(host="192.168.0.5",user="future",pwd="K@ra0Key",db="future")
 import sys
-import time
-import datetime
-import matplotlib.pyplot as plt
+reload(sys)
+sys.setdefaultencoding('utf-8')
+dir = r'C:\Users\YuYang\Desktop\test'
+
+def get_filename_list():
+	filelists=[]
+	files = os.listdir(dir)  
+	for f in files:  
+	    #print  f.decode('gbk') 
+	    filelists.append(f.decode('gbk'))
+	return filelists
+	# for root, dirs, files in os.walk(dir):  
+	#     for name in files:  
+	#         print os.path.join(root, name)  
+
+
+
+
+def pipei_regular(teststring):
+	to_movelist=['Formulas/Custom/Helper/TradeHelper.afl','Formulas/Custom/helper/BacktestHelper.afl','Formulas/Custom/Helper/getPerformance.afl','Formulas/Custom/Helper/TRADEHelper.afl','getPSumTrade','getPSumDay','getPSumMonth','getTradingtimes','getWinRate','getProfit','getMaxDrawdown','AddColumn','Filter=1;','StrategyName =','StrategyID =','ProType = 1;','Trading_log(BSIG,SSIG,CSIG,StrategyID,StrategyName,ProType);','Plot(0,StrategyName,colorBlack,styleNoLine);']
+	istomovw=1
+	for item in to_movelist:
+		if item.lower() in teststring.lower():
+			istomovw=0
+	return istomovw
+
+# pipei_regular()
+
+
+def del_head_info(testlist,StrategyID):
+	filepath=dir+'\\'+testlist
+	filepathnew=dir+'\\'+testlist.split('.')[0]+'_new.afl'
+	# print filepath
+	# print filepathnew
+	isbsig=0
+	with open(filepath,'r') as f:
+		with open(filepathnew,'w') as g:
+			g.write('#include "Formulas/Custom/helper/BacktestHelper.afl";\r')
+			g.write('#include "Formulas/Custom/Helper/TradeHelper.afl";\r')
+			for line in f.readlines():
+				linelist=line.split("\r")
+				for item in linelist:
+					if pipei_regular(item):
+						g.write(item)
+				if 'plotperformance(bsig,ssig,csig);' in line.lower():
+					isbsig=1			
+			#ADD tailf info
+			g.write('StrategyName = "%s";\r' % (testlist.split('.')[0]))
+			g.write('StrategyID = "%s";\r' % (StrategyID))
+			g.write('ProType = 1;\r')
+			g.write('Trading_log(BSIG,SSIG,CSIG,StrategyID,StrategyName,ProType);\r')
+			g.write('Buy = BSIG;\r')
+			g.write('Short = SSIG;\r')
+			g.write('Sell = Cover = CSIG;\r')
+	if isbsig==0:
+		print testlist,"222"
+	shutil.move(filepathnew, filepath)
+
+
+def all_file():
+	StrategyID=99210149
+	filelists=get_filename_list()
+	for item in filelists:
+		if item.split('.')[1]=='afl':
+			del_head_info(item,str(StrategyID))
+			listname=item.split('.')[0].replace("'","")
+			sql="insert into [Future].[dbo].[P_backtest_st_name](st,name,[inserttime]) values('%s','%s',getdate())" % (str(StrategyID),listname)
+			print sql 
+			ms.insert_sql(sql)
+			StrategyID=StrategyID+1
+
+
+
+
+
+
+
+
+
+# del_head_info('10min-MT-rb-30min.afl','99210001')
+all_file()
+
+# for item in filelists:
+# 	print item 
+# # import sys
+# import time
+# import os
+# import datetime
+# import matplotlib.pyplot as plt
 # reload(sys)
 # sys.setdefaultencoding('utf8')
 # from dbconn import MSSQL
-# ms = MSSQL(host="192.168.0.5",user="future",pwd="K@ra0Key",db="future")
-plt.figure(figsize=(8,4))
-x=[2,3,4,5,6]
-y=[3,4,5,6,7]
-aa=x/2
-print aa
+
+# cmd = 'taskkill /F /IM Broker.exe'
+# os.system(cmd)
+# imagename='Broker.exe'
+# p = os.popen('tasklist /FI "IMAGENAME eq %s"' % (imagename))
+# print  p.read().count(imagename)
+
+
+
+
+# def get_filename_list():
+# 	#文件夹不能有中文
+# 	i=0
+# 	filedir="C:\\Users\\YuYang\\Desktop\\test"
+# 	all_files = os.listdir(filedir)
+# 	for file in all_files:
+# 		print file
+# 		i=i+1
+# 		print i 
+
--- pythonfile/new_cal_quayi_forbacktest.py
+++ pythonfile/new_cal_quayi_forbacktest.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.pylab as pl
 from matplotlib.dates import DayLocator, HourLocator, DateFormatter, drange
 ms = MSSQL(host="192.168.0.5",user="future",pwd="K@ra0Key",db="future")
-# resList = ms.find_sql("select top 2 * from st_report")
+# resList = ms.find_sql("select top 2 * from st_report_backtest")
 # print resList
 # 返回行情list 或者0
 
@@ -24,21 +24,16 @@ def input_groupbyquanyi(ac,symbol):
 	try:
 		sql="drop table #temp_quanyi_new"
 		ms.insert_sql(sql)
-		sql="drop table #temp_p_log"
-		ms.insert_sql(sql)
 	except:
 		pass
-	# 产生临时p_log
-	sql="select * into #temp_p_log from (SELECT   aa.*, sid.Symbol, (YEAR(GETDATE()) - 2000) * 10000 + MONTH(GETDATE()) * 100 + DAY(GETDATE()) AS D from (select  p.AC, p.STOCK, p.type, p.ST, p.P_size, a.ratio from P_BASIC p inner join AC_RATIO a on p.AC=a.AC and p.STOCK=a.Stock and p.type=a.type and p.AC='%s') as aa inner join Symbol_ID AS sid ON sid.S_ID = aa.STOCK where Symbol='%s') temp" % (ac,symbol)
-	#print sql
-	ms.insert_sql(sql)
-	sql="select SUM(p_size*ratio/100) as totalsum from #temp_p_log"
+	sql="select SUM(p_size*ratio/100) as totalsum from [Future].[dbo].[backtest_ac_psize] where ac='%s'" % (ac)
+	print sql 
 	res=ms.dict_sql(sql)
 	totalsum=res[0]['totalsum']
 
-	#产生临时整个虚拟组st_report
-	sql="select * into  #temp_quanyi_new from ( select p.ac,p.symbol,st_report.type,st_report.id,st_report.p,st_report.pp,p.p_size,p.ratio ,st_report.st,st_report.stockdate from st_report  inner join #temp_p_log p on p.st=st_report.st and p.ac='%s' and p.symbol='%s')temp " % (ac,symbol)
-	#print sql
+	#产生临时整个虚拟组st_report_backtest
+	sql="select * into  #temp_quanyi_new from ( select p.ac,p.symbol,st_report_backtest.type,st_report_backtest.id,st_report_backtest.p,st_report_backtest.pp,p.p_size,p.ratio ,st_report_backtest.st,st_report_backtest.stockdate from st_report_backtest  inner join [Future].[dbo].[backtest_ac_psize] p on p.st=st_report_backtest.st and p.ac='%s' and p.symbol='%s')temp " % (ac,symbol)
+	print sql
 	ms.insert_sql(sql)
 	#print 1,datetime.datetime.now()
 	sql="select count(1) from #temp_quanyi_new"
@@ -252,7 +247,7 @@ def cal_quanyi(ac,myquotes,totalsum,symbolto):
 
 
 
-(myquotes,totalsum)=input_groupbyquanyi('RB_cvk_strong','RB')
+(myquotes,totalsum)=input_groupbyquanyi('userdefinac','RB')
 #仓位信息OK
 # print totalsum
-cal_quanyi('RB_cvk_strong',myquotes,totalsum,'RB')
+cal_quanyi('userdefinac',myquotes,totalsum,'RB')
