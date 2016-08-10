0e4387f786de9a90285e256364f4303bf8c10e84
wjstzyx <yuyang_998@sina.com>
2016-08-10 17:10:29 +0800

    1

---
+++ pythonfile/delete_multiline.py
@@ -0,0 +1,12 @@
+#coding=utf-8 
+#!/usr/bin/env python
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+# from dbconn import MSSQL
+# ms = MSSQL(host="192.168.0.5",user="future",pwd="K@ra0Key",db="future")
+# resList = ms.find_sql("select top 2 * from st_report")
+# print resList
+
+sourcefile=r"C:\YYfiles\evolutionwork\NewEL\allEl170_0810\1470585600.sgRet.txt"
+newfile=r"C:\YYfiles\evolutionwork\NewEL\targetfile\1470585600.sgRet.txt"
