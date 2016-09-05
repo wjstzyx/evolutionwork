2539acb449dee04bfe38df7674fb6e9c15c6e597
wjstzyx <yuyang_998@sina.com>
2016-09-05 16:05:34 +0800

    1

--- pythonfile/aa.py
+++ pythonfile/aa.py
@@ -8,42 +8,8 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 import datetime
-aa=datetime.datetime(2016,8,9,22,24,20)
-bb=datetime.datetime(2016,8,9,22,24,16,997000)
-print aa
-print bb
-print aa.hour
-
-
-
-
-def add_time_series(totalquanyi,res1):
-	totalquanyitime=[k[0] for k in totalquanyi]
-	res1time=[k[0] for k in res1]
-	totalquanyidict={}
-	for item in totalquanyi:
-		totalquanyidict[item[0]]=item[1]
-	res1dict={}
-	for item in res1:
-		res1dict[item[0]]=item[1]
-	daylist=list(set(totalquanyitime).union(set(res1time)))
-	daylist=sorted(daylist)
-	totalquanyilastvalue=0
-	res1lastvalues=0
-	result={}
-	for item in daylist:
-		tempvalue=0
-		if item in totalquanyitime:
-			totalquanyilastvalue=totalquanyidict[item]
-		if item in res1time:
-			res1lastvalues=res1dict[item]
-		tempvalue=totalquanyilastvalue+res1lastvalues
-		result[item]=tempvalue
-	result=sorted(result.iteritems(), key=lambda d:d[1], reverse = False)
-	print result
-	return result
-
-totalquanyi=[("a11",1),("a13",2),("a15",3)]
-res1=[("a12",10),("a13",20),("a14",300)]
-kkk=add_time_series(totalquanyi,res1)
-add_time_series(kkk,res1)
+###function
+account='account11'
+sql="select * from [LogRecord].[dbo].[order_p_follow]  where ac=F_ac and ac='%s'" % (account)
+res=ms.dict_sql(sql)
+print res 
--- website1/elmanager/elmanager/views.py
+++ website1/elmanager/elmanager/views.py
@@ -98,6 +98,7 @@ def order_account_equity(request):
 	aclist=ms.dict_sql(sql)
 	ICdata=[]
 	ispass=0
+	ishowconfig=0
 	tongji={}
 	account=""
 	res=""
@@ -111,27 +112,35 @@ def order_account_equity(request):
 			account=request.POST.get('searchaccount','')
 			sql="select ID as myid,row_number()OVER(ORDER BY [AC] DESC) as id,ac,F_ac,ratio from [LogRecord].[dbo].[order_p_follow] where ac='%s' order by F_ac" % (account)
 			res=ms.dict_sql(sql)
-			sql="select distinct acname as ac from [LogRecord].[dbo].[quanyicaculatelist] order by acname"
+			#sql="select distinct acname as ac from [LogRecord].[dbo].[quanyicaculatelist] order by acname"
+			sql="select distinct acname as ac,'虚拟组' as type from [LogRecord].[dbo].[quanyicaculatelist] union all select distinct ac,'测试账户' as type from [LogRecord].[dbo].order_p_follow order by type,ac"
 			F_aclist=ms.dict_sql(sql)
 			return render_to_response('order_account_equity.html',{
 				'account':account,
 				'res':res,
 				'F_aclist':F_aclist,
-				'aclist':aclist
+				'aclist':aclist,
+				'ishowconfig':ishowconfig
 			})
 		if whichform=='equity':
+			ishowconfig=1
 			account=request.POST.get('account','')
 			totalquanyiresult=order_get_dailyquanyi(account,150521)
 			if totalquanyiresult['ispass']==0:
 				result=totalquanyiresult['result']
+				configinfo=totalquanyiresult['configinfo']
+				print "22222222",configinfo
 				return render_to_response('order_account_equity.html',{
 					'aclist':aclist,
 					'ispass':ispass,
-					'result':result
+					'result':result,
+					'configinfo':configinfo,
+					'ishowconfig':ishowconfig
 				})
 			else:
 				ispass=1
-				result=totalquanyiresult['result']	
+				result=totalquanyiresult['result']
+				configinfo=totalquanyiresult['configinfo']
 				(tempday,lilunquanyi,realquanyi)=range_series(result,[])
 				tempdict={'acname':account,'symbol':"",'xaxis':tempday,'lilunquanyi':lilunquanyi,'realquanyi':realquanyi}
 				ICdata.append(tempdict)
@@ -145,7 +154,9 @@ def order_account_equity(request):
 		'tongji':tongji,
 		'account':account,
 		'res':res,
-		'F_aclist':F_aclist
+		'F_aclist':F_aclist,
+		'configinfo':configinfo,
+		'ishowconfig':ishowconfig
 	})	
 
 
@@ -1121,7 +1132,7 @@ def get_ac_ratio(account):
 	# print ac_ratio
 	return ac_ratio
 
-#获得账号点菜系统虚拟组
+#获得账号点菜系统虚拟组(第一版本)
 def order_get_ac_ratio(account):
 	#获取总账户配置的虚拟组的ratio
 	ms = MSSQL(host="192.168.0.5",user="future",pwd="K@ra0Key",db="future")
@@ -1148,6 +1159,42 @@ def order_get_ac_ratio(account):
 			del ac_ratio[key]
 	# print ac_ratio
 	return ac_ratio
+
+#获得账号点菜系统虚拟组(第二版本)
+def order_get_ac_ratio_two(account):
+	#获取总账户配置的虚拟组的ratio
+	ms = MSSQL(host="192.168.0.5",user="future",pwd="K@ra0Key",db="future")
+	#sql="WITH Emp AS ( SELECT ac,F_ac,ratio FROM  [LogRecord].[dbo].[order_p_follow] WHERE   ac='%s' UNION ALL  SELECT   D.AC,D.F_ac,D.ratio*emp.ratio/100 FROM   Emp         INNER JOIN [LogRecord].[dbo].[order_p_follow] d ON d.ac = Emp.F_ac)SELECT AC,f_AC,ratio FROM  Emp" % (account)
+	#sql="select AC,f_AC,ratio from [LogRecord].[dbo].[order_p_follow] where ac='%s'" % (account)
+	#检测是否自循环
+	sql="select * from [LogRecord].[dbo].[order_p_follow]  where ac=F_ac and ac='%s'" % (account)
+	res=ms.dict_sql(sql)
+	if res:
+		return []
+
+	sql="WITH Emp AS ( SELECT ac,F_ac,ratio FROM  [LogRecord].[dbo].[order_p_follow] WHERE   ac='%s' UNION ALL  SELECT   D.AC,D.F_ac,D.ratio*emp.ratio/100 FROM   Emp         INNER JOIN [LogRecord].[dbo].[order_p_follow] d ON d.ac = Emp.F_ac)     select '%s' as AC,f_AC,SUM(ratio) as ratio from Emp where  f_ac not in (select ac from Emp) group by F_ac" % (account,account)
+	res=ms.dict_sql(sql)
+	accountlist=[]
+	aclist=[]
+	for item in res:
+		accountlist.append(item['AC'])
+		aclist.append(item['f_AC'])
+	accountlist=list(set(accountlist))
+	aclist=list(set(aclist))
+	# print accountlist
+	# print aclist
+	ac_ratio={}
+	for item in aclist:
+		ac_ratio[item]=0
+	for item in res:
+		ac_ratio[item['f_AC']]=ac_ratio[item['f_AC']]+item['ratio']
+	# print ac_ratio
+	for key in accountlist:
+		if ac_ratio.has_key(key):
+			del ac_ratio[key]
+	# print ac_ratio
+	return ac_ratio
+
 
 
 #计算总权益
@@ -1189,20 +1236,32 @@ def get_dailyquanyi(account,fromDdy):
 #计算总权益(点菜)
 def order_get_dailyquanyi(account,fromDdy):
 	ms = MSSQL(host="192.168.0.5",user="future",pwd="K@ra0Key",db="future")
-	ac_ratio=order_get_ac_ratio(account)
+	ac_ratio=order_get_ac_ratio_two(account)
 	totalquanyi=[]
 	#获取对齐时间
 	Dlist=[]
+	tempresult=""
+	configinfo=[]
 	Dlist.append(fromDdy)
+	if ac_ratio==[]:
+		return {"ispass":0,"result":"存在有自己跟随自己的配置，请修正"}
 	for key in ac_ratio:
 		sql="SELECT top 1  (convert(int,replace(convert(varchar(10),DATEADD(day,1,stockdate),120),'-',''))-20000000) as D  FROM [Future].[dbo].[quanyi_log_groupby_v2] where ac='%s' order by stockdate" % (key)
 		tempD=ms.dict_sql(sql)
 		if tempD:
 			Dlist.append(tempD[0]['D'])
+			configinfo.append([key,ac_ratio[key],tempD[0]['D']])
 		else:
 			Dlist.append(200000)
+			sql="select ac from [LogRecord].[dbo].[order_p_follow]  where F_ac='%s'" % (key)
+			tempres=ms.dict_sql(sql)
+			tempresult=tempresult+" 基本账户 %s 中 %s 没有产生过信号,请补全近两年策略信号" % (tempres[0]['ac'],key)
+			configinfo.append([key,ac_ratio['key'],200000])
 	fromDdy=max(Dlist)
 	print Dlist
+	if 200000 in Dlist:
+		return {"ispass":0,"result":tempresult,"configinfo":configinfo}
+
 		
 	for key in ac_ratio:
 		ratio=ac_ratio[key]
@@ -1232,7 +1291,7 @@ def order_get_dailyquanyi(account,fromDdy):
 	totalquanyi=[[item[1],item[0]] for item in totalquanyi]
 				# for item in totalquanyi:
 				# 	print item 
-	return {"ispass":1,"result":totalquanyi}
+	return {"ispass":1,"result":totalquanyi,"configinfo":configinfo}
 
 
 
