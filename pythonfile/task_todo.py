846cadf165f2fd6b85561fa3abc0921eef81acb7
wjstzyx <yuyang_998@sina.com>
2016-09-22 17:40:30 +0800

    1

---
+++ pythonfile/task_todo.py
@@ -0,0 +1,30 @@
+#coding=utf-8 
+#!/usr/bin/env python
+import urllib, urllib2, json
+import sys
+import datetime
+reload(sys)
+sys.setdefaultencoding('utf8')
+from dbconn import MSSQL
+ms = MSSQL(host="192.168.0.5",user="future",pwd="K@ra0Key",db="future")
+# resList = ms.find_sql("select top 2 * from st_report")
+# print resList
+# -*- coding: utf-8 -*-
+
+
+def istasktodo(type):
+	sql="select * FROM [LogRecord].[dbo].[task_todo] where type='%s' and status=0 " % (type)
+	res=ms.dict_sql(sql)
+	nowtime=datetime.datetime.now().strftime('%H%M')
+	print nowtime
+	for item in res:
+		if nowtime>=item['todotime']:
+			cmd=item['cmd']
+			print '%s &' % (cmd)
+			os.system('%s &' % (cmd))
+
+
+		print item 
+
+istasktodo('datafix')
+
