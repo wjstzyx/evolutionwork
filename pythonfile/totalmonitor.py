2b84b985cd2c84d1c81cac5e8f5fbe1f7ee484d8
wjstzyx <yuyang_998@sina.com>
2016-09-30 11:17:30 +0800

    1

--- pythonfile/totalmonitor.py
+++ pythonfile/totalmonitor.py
@@ -15,7 +15,7 @@ ms = MSSQL(host="192.168.0.5",user="future",pwd="K@ra0Key",db="future")
 #采集行情的心跳监测
 #监控时间段：在crontab 设置 08:00---23:00,延迟2分钟报警
 def monitor():
-	sql="select DATEDIFF(MINUTE, '2016-09-30 11:00:00', getdate()) as aaa from [LogRecord].[dbo].[quotes_python_heart]where type='quotes_python' and isactive=1"
+	sql="select DATEDIFF(MINUTE, updatetime, getdate()) as aaa from [LogRecord].[dbo].[quotes_python_heart]where type='quotes_python' and isactive=1"
 	res=ms.dict_sql(sql)
 	if res[0]['aaa']>=2:
 		sendmessage='13764504303'
