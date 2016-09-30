222cc01091e81404edb3d06849bc15a4c227da84
wjstzyx <yuyang_998@sina.com>
2016-09-30 11:16:25 +0800

    1

--- pythonfile/totalmonitor.py
+++ pythonfile/totalmonitor.py
@@ -1,7 +1,8 @@
 #coding=utf-8 
 #!/usr/bin/env python
 import sys, urllib, urllib2, json
-import sys
+import datetime
+from sendSMSM import sendsms
 reload(sys)
 sys.setdefaultencoding('utf8')
 from dbconn import MSSQL
@@ -11,14 +12,20 @@ ms = MSSQL(host="192.168.0.5",user="future",pwd="K@ra0Key",db="future")
 # -*- coding: utf-8 -*-
 
 
-url = 'http://apis.baidu.com/xiaogg/holiday/holiday?d=20160902'
+#采集行情的心跳监测
+#监控时间段：在crontab 设置 08:00---23:00,延迟2分钟报警
+def monitor():
+	sql="select DATEDIFF(MINUTE, '2016-09-30 11:00:00', getdate()) as aaa from [LogRecord].[dbo].[quotes_python_heart]where type='quotes_python' and isactive=1"
+	res=ms.dict_sql(sql)
+	if res[0]['aaa']>=2:
+		sendmessage='13764504303'
+		totalsubject="行情脚本没有心跳"
+		smsresult=sendsms(sendmessage,totalsubject)
+		# print smsresult
 
 
-req = urllib2.Request(url)
 
-req.add_header("apikey", "6677fe3debefda50dd040fbf98d0d38b")
+monitor()
 
-resp = urllib2.urlopen(req)
-content = resp.read()
-if(content):
-    print(content)
+
+
