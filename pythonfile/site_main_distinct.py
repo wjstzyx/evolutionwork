67677a5bc135338f99553072e6cba052c051cefd
wjstzyx <yuyang_998@sina.com>
2017-03-07 16:58:48 +0800

    1

--- pythonfile/site_main_distinct.py
+++ pythonfile/site_main_distinct.py
@@ -9,6 +9,7 @@ import numpy as np
 reload(sys)
 sys.setdefaultencoding('utf8')
 from dbconn import MSSQL
+import multiprocessing
 ms = MSSQL(host="192.168.0.5",user="future",pwd="K@ra0Key",db="future")
 # resList = ms.find_sql("select top 2 * from st_report")
 # print resList
@@ -16,8 +17,34 @@ ms = MSSQL(host="192.168.0.5",user="future",pwd="K@ra0Key",db="future")
 
 
 
-	stepmultilist=['StepMultigaosheng1','StepMultiI300w_up','StepMultituji1','StepMultituji2','StepMultituji3','StepMultidnhiboth','StepMultidnhiprofit','StepMultidnhisharp','StepMultidnshort']
-for item in stepmultilist:
-	#cmd="python site_generate_dissitnct_V2.py %s" % (item)
+stepmultilist=['StepMultigaosheng1','StepMultiI300w_up','StepMultituji1','StepMultituji2','StepMultituji3','StepMultidnhiboth','StepMultidnhiprofit','StepMultidnhisharp','StepMultidnshort']
+
+
+def sub_main(item):
+	cmd = "python site_generate_dissitnct_V4.py %s" % (item)
+	os.system(cmd)
 	cmd = "python site_acname_zhuli_equity.py %s" % (item)
 	os.system(cmd)
+
+if __name__ == "__main__":
+	threads_N = 10
+	multiprocessing.freeze_support()
+	pool = multiprocessing.Pool(processes=threads_N)
+
+	stepmultilist = ['StepMultidnhiboth', 'StepMultidnshort', 'StepMultidnhiprofit', 'StepMultidnhisharp',
+	                 'StepMultigaosheng1', 'StepMultiI300w_up', 'StepMultituji1', 'StepMultituji2', 'StepMultituji3']
+	for item in stepmultilist:
+		# add process to pool
+		if threads_N > 1:
+			# print multiprocessing.current_process().name
+			pool.apply_async(sub_main, (item,))
+		else:
+			sub_main(item, )
+
+	print "Process Pool Closed, Waiting for sub Process Finishing..."
+	pool.close()
+	pool.join()
+
+	print 'Finished'
+
+
